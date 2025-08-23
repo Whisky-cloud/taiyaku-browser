ad406a81c5c32487bb70dd161fe1e2683621ba37
Whisky-cloud <p2pm751l@gmail.com>
2025-08-23 20:17:08 +0900

    batchSize = 1

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get("/api/translate-stream", async (req, res) => {
       pageCache[url] = sentences;
     }
 
-    const batchSize = 3;
+    const batchSize = 1;
     const maxBatchSentences = 100;
     const end = Math.min(sentences.length, start + maxBatchSentences);
 
